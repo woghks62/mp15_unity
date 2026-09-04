6a073ec52073034302c5dd08e6861eb40814d696
woghks62 <p2pee@naver.com>
2026-09-04 11:48:51 +0900

    [수업] Collision

--- My project (1)/Assets/date0904/CubeController.cs
+++ My project (1)/Assets/date0904/CubeController.cs
@@ -11,26 +11,50 @@ public class CubeController : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    
-    // Rigidbody
-    private void Update()
+    // Collision (충돌)
+    // - Enter : 충돌 시작했을 때
+    // - Stay : 충돌이 유지되고 있을 때
+    // - Exit : 충돌이 종료됐을 때
+
+    private void OnCollisionEnter(Collision collision)
     {
-        Foo();
+        Debug.Log($"{gameObject.name} : {collision.gameObject.name}랑 충돌했다");
     }
 
-    void Foo()
+    private void OnCollisionStay(Collision collision)
     {
-        if (!Input.GetKey(KeyCode.Space)) return;
-
-
-        // AddForce();
-        //if (!Input.GetKeyDown(KeyCode.Space)) return;
-        //_rigidbody.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
-
-        // velocity;
-        // if (!Input.GetKey(KeyCode.Space)) return;
-        //_rigidbody.velocity = new Vector3(0, 0, 5);
-        _rigidbody.angularVelocity = new Vector3(0, 0, 5);
-        
+        Debug.Log($"{gameObject.name} : {collision.gameObject.name}랑 붙어있음");
     }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log($"{gameObject.name} : {collision.gameObject.name}랑 멀어짐");
+    }
+
+
+    // Trigger (스위치)
+
+
+
+    //// Rigidbody
+    //private void Update()
+    //{
+    //    Foo();
+    //}
+
+    //void Foo()
+    //{
+    //    if (!Input.GetKey(KeyCode.Space)) return;
+
+
+    //    // AddForce();
+    //    //if (!Input.GetKeyDown(KeyCode.Space)) return;
+    //    //_rigidbody.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+
+    //    // velocity;
+    //    // if (!Input.GetKey(KeyCode.Space)) return;
+    //    //_rigidbody.velocity = new Vector3(0, 0, 5);
+    //    _rigidbody.angularVelocity = new Vector3(0, 0, 5);
+
+    //}
 }
